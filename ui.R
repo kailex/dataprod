147830eb66eb9179f42ac639099a6b86889b42c0
kailex <kailex@users.noreply.github.com>
2014-06-17 14:43:16 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,7 +10,16 @@ shinyUI(pageWithSidebar(
     radioButtons("am", "Transmission:", c("Automatic"=0, "Manual"=1))
   ),
   mainPanel(
-    h3("mpg ~ wt + qsec + am"),
+    h4("
+       This simple application allows to calculate miles per gallon for a particular car.
+       To do this we need to input a weight of car and 1/4 mile time using sliders. 
+       Also we may choose a type of a transmission with radio button. 
+       All input elements are situated on the left side of a page. 
+       On the right side of the page there are two plots indicating dependency between MPG,
+       weight, 1/4 mile time and type of transmission.
+     
+       We use a linear model which was built w.r.t. the AIC score:"),
+    h4("mpg ~ wt + qsec + am"),
     plotOutput("plots")
   )
 ))
